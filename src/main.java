aba1b505eb9c9edd70e89648854795827fefd6a3
Neto test <krayzerexe@yandex.ru>
2024-04-14 13:37:40 +0300

    main

--- src/main.java
+++ src/main.java
@@ -1,3 +1,16 @@
 public class main {
-
+    public static void main(String[] args) {
+        int score = 200;
+        int replenishment = 3000;
+        int bonus;
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        ;
+        System.out.println(bonus);
+        int amount = score + replenishment + bonus;
+        System.out.println(amount);
+    }
 }
